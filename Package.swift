1480584d88c8f07863c36d57c1d78cc898646e69
parkdigy <parkdigy@gmail.com>
2025-03-22 15:57:45 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "NasRunKit",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "NasRunKit",
